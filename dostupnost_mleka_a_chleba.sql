ea2e3441f5482710837647bf669e4ebc8ca3135a
VeronikCS288 <sustova.veronika@seznam.cz>
2025-01-12 10:19:35 +0100

    Update dostupnost_mleka_a_chleba.sql

--- dostupnost_mleka_a_chleba.sql
+++ dostupnost_mleka_a_chleba.sql
@@ -13,7 +13,6 @@ SELECT
 	LEFT JOIN t_veronika_sustova_project_sql_primary_final bread_data ON source_data.YEAR = bread_data.YEAR AND source_data.industry_branch_code = bread_data.industry_branch_code AND bread_data.category = 111301
 	where source_data.category = 114201
 ) SELECT 
-
 	pricesToIncomeRatio.industry_branch_name AS "odvětví",
 	pricesToIncomeRatio.YEAR AS "rok",
 	pricesToIncomeRatio.income AS "příjem",
